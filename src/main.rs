38550c2279cde7965c84f548d2a258b3ace217b6
Dmitry Konishchev <konishchev@gmail.com>
2018-12-12 23:05:49 +0300

    Rust 2018: Drop `extern crate diesel_derive_enum`

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
-// FIXME: Drop?
 #[macro_use] extern crate diesel;
-#[macro_use] extern crate diesel_derive_enum;
 #[macro_use] extern crate diesel_migrations;
 #[macro_use] extern crate serde_derive;
 
