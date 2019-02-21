8529c82c7bc46d8ae964d6a884903d77f6250fec
Dmitry Konishchev <konishchev@gmail.com>
2019-02-21 22:57:33 +0300

    Introduce TaxIdExtractor

--- src/broker_statement/dividends.rs
+++ src/broker_statement/dividends.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 use crate::core::GenericResult;
 use crate::currency::Cash;
 use crate::currency::converter::CurrencyConverter;
+use crate::formatting;
 use crate::localities::Country;
 use crate::types::{Date, Decimal};
 
@@ -25,23 +26,45 @@ impl Dividend {
 }
 
 pub struct DividendWithoutPaidTax {
-    date: Date,
-    issuer: String,
-    amount: Cash,
-    tax_extractor: Box<DividendPaidTaxExtractor>,
+    pub date: Date,
+    pub issuer: String,
+    pub amount: Cash,
+    pub tax_extractor: Box<DividendPaidTaxExtractor>,
 }
 
 impl DividendWithoutPaidTax {
     pub fn upgrade(self, taxes: &mut HashMap<TaxId, Cash>) -> GenericResult<Dividend> {
+        let paid_tax = self.tax_extractor.get_paid_tax(taxes).map_err(|e| format!(
+            "Unable to match {} dividend from {} to paid taxes: {}",
+            self.issuer, formatting::format_date(self.date), e))?;
+
         Ok(Dividend {
             date: self.date,
             issuer: self.issuer,
             amount: self.amount,
-            paid_tax: self.tax_extractor.get_paid_tax(taxes)?,
+            paid_tax: paid_tax,
         })
     }
 }
 
 pub trait DividendPaidTaxExtractor {
     fn get_paid_tax(&self, taxes: &mut HashMap<TaxId, Cash>) -> GenericResult<Cash>;
+}
+
+pub struct TaxIdExtractor {
+    tax_id: TaxId,
+}
+
+impl TaxIdExtractor {
+    pub fn new(tax_id: TaxId) -> Box<DividendPaidTaxExtractor> {
+        Box::new(TaxIdExtractor {tax_id: tax_id})
+    }
+}
+
+impl DividendPaidTaxExtractor for TaxIdExtractor {
+    fn get_paid_tax(&self, taxes: &mut HashMap<TaxId, Cash>) -> GenericResult<Cash> {
+        Ok(taxes.remove(&self.tax_id).ok_or_else(|| format!(
+            "There is no tax with {:?} expected description", self.tax_id.description
+        ))?)
+    }
 }
