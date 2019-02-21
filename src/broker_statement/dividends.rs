88300af396904326a462fea61026bd5e1b8e0c4e
Dmitry Konishchev <konishchev@gmail.com>
2019-02-21 14:11:19 +0300

    Move Dividend to a separate module

---
+++ src/broker_statement/dividends.rs
@@ -0,0 +1,21 @@
+use crate::core::GenericResult;
+use crate::currency::Cash;
+use crate::currency::converter::CurrencyConverter;
+use crate::localities::Country;
+use crate::types::{Date, Decimal};
+
+#[derive(Debug)]
+pub struct Dividend {
+    pub date: Date,
+    pub issuer: String,
+    pub amount: Cash,
+    pub paid_tax: Cash,
+}
+
+impl Dividend {
+    pub fn tax_to_pay(&self, country: &Country, converter: &CurrencyConverter) -> GenericResult<Decimal> {
+        let amount = converter.convert_to(self.date, self.amount, country.currency)?;
+        let paid_tax = converter.convert_to(self.date, self.paid_tax, country.currency)?;
+        Ok(country.tax_to_pay(amount, Some(paid_tax)))
+    }
+}
--- src/broker_statement/mod.rs
+++ src/broker_statement/mod.rs
@@ -1,3 +1,4 @@
+mod dividends;
 mod ib;
 mod open_broker;
 mod partial;
@@ -21,6 +22,7 @@ use crate::localities::Country;
 use crate::types::{Date, Decimal};
 use crate::util;
 
+pub use self::dividends::Dividend;
 use self::partial::PartialBrokerStatement;
 
 #[derive(Debug)]
@@ -488,21 +490,5 @@ impl StockSell {
         }
 
         Ok(country.tax_to_pay(income, None))
-    }
-}
-
-#[derive(Debug)]
-pub struct Dividend {
-    pub date: Date,
-    pub issuer: String,
-    pub amount: Cash,
-    pub paid_tax: Cash,
-}
-
-impl Dividend {
-    pub fn tax_to_pay(&self, country: &Country, converter: &CurrencyConverter) -> GenericResult<Decimal> {
-        let amount = converter.convert_to(self.date, self.amount, country.currency)?;
-        let paid_tax = converter.convert_to(self.date, self.paid_tax, country.currency)?;
-        Ok(country.tax_to_pay(amount, Some(paid_tax)))
     }
 }
