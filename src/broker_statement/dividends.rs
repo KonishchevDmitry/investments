ff1fc841dcf893952749774000fc8c2a1b610185
Dmitry Konishchev <konishchev@gmail.com>
2019-02-21 15:42:23 +0300

    Introduce DividendWithoutPaidTax

--- src/broker_statement/dividends.rs
+++ src/broker_statement/dividends.rs
@@ -1,8 +1,12 @@
+use std::collections::HashMap;
+
 use crate::core::GenericResult;
 use crate::currency::Cash;
 use crate::currency::converter::CurrencyConverter;
 use crate::localities::Country;
 use crate::types::{Date, Decimal};
+
+use super::TaxId;
 
 #[derive(Debug)]
 pub struct Dividend {
@@ -18,4 +22,26 @@ impl Dividend {
         let paid_tax = converter.convert_to(self.date, self.paid_tax, country.currency)?;
         Ok(country.tax_to_pay(amount, Some(paid_tax)))
     }
+}
+
+pub struct DividendWithoutPaidTax {
+    date: Date,
+    issuer: String,
+    amount: Cash,
+    tax_extractor: Box<DividendPaidTaxExtractor>,
+}
+
+impl DividendWithoutPaidTax {
+    pub fn upgrade(self, taxes: &mut HashMap<TaxId, Cash>) -> GenericResult<Dividend> {
+        Ok(Dividend {
+            date: self.date,
+            issuer: self.issuer,
+            amount: self.amount,
+            paid_tax: self.tax_extractor.get_paid_tax(taxes)?,
+        })
+    }
+}
+
+pub trait DividendPaidTaxExtractor {
+    fn get_paid_tax(&self, taxes: &mut HashMap<TaxId, Cash>) -> GenericResult<Cash>;
 }
