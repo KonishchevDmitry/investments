132bdf5f86bcd7c7260533f7b21538b8488bb057
Dmitry Konishchev <konishchev@gmail.com>
2019-12-15 19:51:32 +0300

    BCS instrument parsing to symbol

--- src/broker_statement/bcs/common.rs
+++ src/broker_statement/bcs/common.rs
@@ -1,3 +1,6 @@
+use lazy_static::lazy_static;
+use regex::Regex;
+
 use crate::core::GenericResult;
 use crate::types::Date;
 use crate::util;
@@ -19,4 +22,27 @@ pub fn map_currency(name: &str) -> Option<&'static str> {
 
 pub fn parse_currency(name: &str) -> GenericResult<&'static str> {
     Ok(map_currency(name).ok_or_else(|| format!("Unsupported currency: {:?}", name))?)
+}
+
+pub fn parse_symbol(name: &str) -> GenericResult<String> {
+    lazy_static! {
+        static ref SYMBOL_REGEX: Regex = Regex::new(
+            r"^(?P<symbol>[A-Z]+)(?:[._][A-Z]+)?$").unwrap();
+    }
+
+    let captures = SYMBOL_REGEX.captures(name).ok_or_else(|| format!(
+        "Invalid instrument symbol: {:?}", name))?;
+
+    Ok(captures.name("symbol").unwrap().as_str().to_owned())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn symbol_parsing() {
+        assert_eq!(parse_symbol("FXRL_RX").unwrap(), s!("FXRL"));
+        assert_eq!(parse_symbol("FXRU.MRG").unwrap(), s!("FXRU"));
+    }
 }
