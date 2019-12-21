67fe7d64bcf752a199dbd78910b8491910bf8c8f
Dmitry Konishchev <konishchev@gmail.com>
2019-12-21 23:35:37 +0300

    Start implementing complex commission spec

--- src/commissions.rs
+++ src/commissions.rs
@@ -6,8 +6,6 @@ use crate::core::GenericResult;
 use crate::currency::Cash;
 use crate::types::{Date, Decimal, TradeType};
 
-use super::get_trade_volume;
-
 struct TieredCommissionSpec {
     currency: &'static str,
     tiers: Vec<CommissionTier>,
@@ -42,60 +40,32 @@ impl TierCommissionSpec {
 
 struct TieredCommissionCalc {
     spec: TieredCommissionSpec,
-    state: HashMap<Date, State>,
+    volume: HashMap<Date, Decimal>,
 }
 
 impl TieredCommissionCalc {
     fn new(spec: TieredCommissionSpec) -> TieredCommissionCalc {
         TieredCommissionCalc {
             spec,
-            state: HashMap::new(),
+            volume: HashMap::new(),
         }
     }
 
     fn add_trade(
         &mut self, date: Date, _trade_type: TradeType, shares: u32, price: Cash
     ) -> GenericResult<Cash> {
-        let state = self.state.entry(date).or_insert_with(|| {
-            State {
-                tier: 0,
-                volume: dec!(0),
-                commission: dec!(0),
-            }
-        });
-        let mut remaining_volume = get_trade_volume(self.spec.currency, price * shares)?;
-
-        while remaining_volume != dec!(0) {
-            assert!(remaining_volume > dec!(0));
-
-            if state.tier == self.spec.tiers.len() - 1 {
-                state.add_volume(&self.spec, remaining_volume);
-                break;
-            }
-            let next_tier_volume = self.spec.tiers[state.tier + 1].start_volume;
-
-            if state.volume + remaining_volume < next_tier_volume {
-                state.add_volume(&self.spec, remaining_volume);
-                break;
-            }
-
-            let current_volume = next_tier_volume - state.volume;
-            state.add_volume(&self.spec, current_volume);
-            remaining_volume -= current_volume;
-            state.tier += 1;
-        }
-
-        assert!(state.volume >= self.spec.tiers[state.tier].start_volume);
-        if state.tier < self.spec.tiers.len() - 1 {
-            assert!(state.volume < self.spec.tiers[state.tier + 1].start_volume);
-        }
-
+        let volume = get_trade_volume(self.spec.currency, price * shares)?;
+        *self.volume.entry(date).or_default() += volume;
         Ok(Cash::new(self.spec.currency, dec!(0)))
     }
 
     fn calculate(self) -> HashMap<Date, Cash> {
-        self.state.iter().map(|(&date, state)| {
-            let mut commission = state.commission;
+        self.volume.iter().map(|(&date, &volume)| {
+            let commission_spec = &self.spec.tiers.iter().filter(|tier| {
+                tier.start_volume <= volume
+            }).last().unwrap().commission_spec;
+
+            let mut commission = volume * commission_spec.percent / dec!(100);
 
             if let Some(minimum) = self.spec.minimum_daily {
                 if commission < minimum {
@@ -103,23 +73,30 @@ impl TieredCommissionCalc {
                 }
             }
 
+            // FIXME: Parametrize
+            // It seems that BCS truncates commission instead of rounding, so do the same
+            let scale = commission.scale();
+            if scale > 2 {
+                commission.set_scale(scale - 2);
+                commission = commission.trunc();
+                commission.set_scale(2);
+            }
+
             (date, Cash::new(self.spec.currency, commission))
         }).collect()
     }
 }
 
-struct State {
-    tier: usize,
-    volume: Decimal,
-    commission: Decimal,
-}
-
-impl State {
-    fn add_volume(&mut self, commission_spec: &TieredCommissionSpec, volume: Decimal) {
-        let spec = &commission_spec.tiers[self.tier].commission_spec;
-        self.volume += volume;
-        self.commission += volume * spec.percent / dec!(100);
+fn get_trade_volume(commission_currency: &str, volume: Cash) -> GenericResult<Decimal> {
+    if volume.currency != commission_currency {
+        return Err!(concat!(
+            "Unable to calculate trade commission: ",
+            "Commission currency doesn't match trade currency: {} vs {}"),
+            commission_currency, volume.currency
+        );
     }
+
+    Ok(volume.amount)
 }
 
 #[cfg(test)]
@@ -154,7 +131,7 @@ mod tests {
             (date!(2, 12, 2019),  35, dec!(2959.5)),
             (date!(2, 12, 2019),   3, dec!(2960)),
             (date!(2, 12, 2019),  18, dec!(2960)),
-//            (date!(3, 12, 2019), 107, dec!( 782.4)),
+            (date!(3, 12, 2019), 107, dec!( 782.4)),
         ] {
             assert_eq!(
                 commission_calc.add_trade(date, trade_type, shares, Cash::new(currency, price)).unwrap(),
@@ -163,8 +140,8 @@ mod tests {
         }
 
         assert_eq!(commission_calc.calculate(), hashmap!{
-            date!(2, 12, 2019) => Cash::new(currency, dec!(68.45) + dec!(16.57)),
-//            date!(3, 12, 2019) => Cash::new(currency, dec!(44.45) + dec!(8.37)),
+            date!(2, 12, 2019) => Cash::new(currency, dec!(85.02)),
+            date!(3, 12, 2019) => Cash::new(currency, dec!(52.82)),
         });
     }
 }
