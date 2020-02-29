710f9dcbc52f3855d2ccdf5ed35235c7be2000c9
Dmitry Konishchev <konishchev@gmail.com>
2020-02-29 12:23:21 +0300

    A temporary hack for the specific tax reclassification case until we'll be able to handle such cases

--- src/broker_statement/ib/taxes.rs
+++ src/broker_statement/ib/taxes.rs
@@ -9,6 +9,19 @@ use crate::util::DecimalRestrictions;
 use super::StatementParser;
 use super::common::{Record, RecordParser, parse_date};
 
+// Every year IB has to adjust the 1042 withholding (i.e. withholding on US dividends paid to non-US
+// accounts) to reflect dividend reclassifications. This is typically done in February the following
+// year. As such, the majority of these adjustments are refunds to customers. The typical case is
+// when IB's best information at the time of paying a dividend indicates that the distribution is an
+// ordinary dividend (and therefore subject to withholding), then later at year end, the dividend is
+// reclassified as Return of Capital, proceeds, or capital gains (all of which are not subject to
+// 1042 withholding).
+//
+// So withholding in previous year's statements should be reviewed against February statement's
+// withholding adjustments. As it turns out, dates may not match.
+//
+// At this time we match dividends on taxes using (date, symbol) pair. Matching by description
+// turned out to be too fragile.
 pub struct WithholdingTaxParser {}
 
 impl RecordParser for WithholdingTaxParser {
@@ -18,8 +31,16 @@ impl RecordParser for WithholdingTaxParser {
 
     fn parse(&self, parser: &mut StatementParser, record: &Record) -> EmptyResult {
         let currency = record.get_value("Currency")?;
-        let date = parse_date(record.get_value("Date")?)?;
+        let mut date = parse_date(record.get_value("Date")?)?;
         let issuer = parse_tax_description(record.get_value("Description")?)?;
+
+        // FIXME: A temporary hack for the specific tax reclassification case until we'll be able
+        // to handle such cases.
+        if date == date!(13, 2, 2020) && record.get_value("Description")? ==
+            "BND(US9219378356) Cash Dividend 0.19834500 USD per Share - US Tax" {
+            date = date!(6, 2, 2019);
+        }
+
         let tax_id = TaxId::new(date, &issuer);
 
         // Tax amount is represented as a negative number.
