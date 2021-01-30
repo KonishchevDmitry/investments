f521038776e6f508a0a7786fa210ef5d0f64db38
Dmitry Konishchev <konishchev@gmail.com>
2021-01-30 15:48:43 +0300

    Add test for trades with currency different from broker plan's base currency (#25)

--- src/brokers/plans/tinkoff.rs
+++ src/brokers/plans/tinkoff.rs
@@ -8,6 +8,16 @@ use crate::commissions::{
 #[cfg(test)] use crate::currency::converter::CurrencyConverter;
 #[cfg(test)] use crate::types::TradeType;
 
+pub fn investor() -> CommissionSpec {
+    CommissionSpecBuilder::new("RUB")
+        .trade(TradeCommissionSpecBuilder::new()
+            .commission(TransactionCommissionSpecBuilder::new()
+                .percent(dec!(0.3))
+                .build().unwrap())
+            .build())
+        .build()
+}
+
 // Please note:
 // We don't support Tinkoff volume tiers: actual commission depends on the order of trades which is
 // inappropriate for our purposes.
@@ -43,6 +53,30 @@ mod tests {
     use rstest::rstest;
     use super::*;
 
+    #[rstest(trade_type => [TradeType::Buy, TradeType::Sell])]
+    fn investor(trade_type: TradeType) {
+        let converter = CurrencyConverter::mock();
+        let mut calc = CommissionCalc::new(
+            converter, super::investor(), Cash::new("RUB", dec!(0))).unwrap();
+
+        let date = date!(27, 7, 2020);
+
+        for &(currency, quantity, price, commission) in &[
+            ("RUB", 30, dec!(184.69), dec!(16.62)),
+            ("USD",  1, dec!( 50.48), dec!( 0.15)),
+            ("RUB", 20, dec!(184.84), dec!(11.09)),
+            ("USD",  1, dec!( 50.22), dec!( 0.15)),
+            ("RUB", 10, dec!(201.43), dec!( 6.04)),
+        ] {
+            assert_eq!(
+                calc.add_trade(date, trade_type, quantity.into(), Cash::new(currency, price)).unwrap(),
+                Cash::new(currency, commission),
+            );
+        }
+
+        assert!(calc.calculate().unwrap().is_empty());
+    }
+
     #[rstest(
         portfolio_net_value_tiers => [
             (-1, 290), (0, 290), (1_999_999, 290),
@@ -54,36 +88,24 @@ mod tests {
         let (portfolio_net_value, depositary) = portfolio_net_value_tiers;
 
         let currency = "RUB";
+        let date = date!(22, 6, 2020);
+
         let converter = CurrencyConverter::mock();
         let mut calc = CommissionCalc::new(
             converter, super::trader(), Cash::new(currency, portfolio_net_value.into())).unwrap();
 
-        let date = date!(22, 6, 2020);
-
-        assert_eq!(
-            calc.add_trade(date, trade_type, 22.into(), Cash::new(currency, dec!(3890))).unwrap(),
-            Cash::new(currency, dec!(42.79)),
-        );
-
-        assert_eq!(
-            calc.add_trade(date, trade_type, 3.into(), Cash::new(currency, dec!(3124))).unwrap(),
-            Cash::new(currency, dec!(4.69)),
-        );
-
-        assert_eq!(
-            calc.add_trade(date, trade_type, 3.into(), Cash::new(currency, dec!(2809.5))).unwrap(),
-            Cash::new(currency, dec!(4.21)),
-        );
-
-        assert_eq!(
-            calc.add_trade(date, trade_type, 3.into(), Cash::new(currency, dec!(2196))).unwrap(),
-            Cash::new(currency, dec!(3.29)),
-        );
-
-        assert_eq!(
-            calc.add_trade(date, trade_type, 45.into(), Cash::new(currency, dec!(864.4))).unwrap(),
-            Cash::new(currency, dec!(19.45)),
-        );
+        for &(quantity, price, commission) in &[
+            (22, dec!(3890  ), dec!(42.79)),
+            ( 3, dec!(3124  ), dec!( 4.69)),
+            ( 3, dec!(2809.5), dec!( 4.21)),
+            ( 3, dec!(2196  ), dec!( 3.29)),
+            (45, dec!( 864.4), dec!(19.45)),
+        ] {
+            assert_eq!(
+                calc.add_trade(date, trade_type, quantity.into(), Cash::new(currency, price)).unwrap(),
+                Cash::new(currency, commission),
+            );
+        }
 
         let mut additional = HashMap::new();
         if depositary != 0 {
@@ -92,5 +114,37 @@ mod tests {
             additional.insert(date!(1, 7, 2020), commissions);
         }
         assert_eq!(calc.calculate().unwrap(), additional);
+    }
+
+    #[rstest(trade_type => [TradeType::Buy, TradeType::Sell])]
+    fn trader_mixed_currency(trade_type: TradeType) {
+        let converter = CurrencyConverter::mock();
+        let mut calc = CommissionCalc::new(
+            converter, super::trader(), Cash::new("RUB", dec!(0))).unwrap();
+
+        let date = date!(10, 12, 2020);
+        assert_eq!(
+            calc.add_trade(date, trade_type, 100.into(), Cash::new("RUB", dec!(8.09))).unwrap(),
+            Cash::new("RUB", dec!(0.4)),
+        );
+
+        let date = date!(18, 12, 2020);
+        for &(currency, quantity, price, commission) in &[
+            ("RUB", 100, dec!(8.125), dec!(0.41)),
+            ("USD",   1, dec!(15.78), dec!(0.01)),
+            ("USD",   6, dec!(15.81), dec!(0.05)),
+            ("USD",   2, dec!(15.81), dec!(0.02)),
+        ] {
+            assert_eq!(
+                calc.add_trade(date, trade_type, quantity.into(), Cash::new(currency, price)).unwrap(),
+                Cash::new(currency, commission),
+            );
+        }
+
+        assert_eq!(calc.calculate().unwrap(), hashmap!{
+            // Depositary commission
+            // Actually the date is the date of the first trade
+            date!(1, 1, 2021) => MultiCurrencyCashAccount::new_from(Cash::new("RUB", dec!(290))),
+        });
     }
 }
