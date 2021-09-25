3d5817a32d4fa72ebc52be24f60aa861e7c31fb6
Dmitry Konishchev <konishchev@gmail.com>
2021-09-25 17:13:26 +0300

    Parse SPB cash assets (#19)

---
+++ src/broker_statement/open/spb/cash_assets.rs
@@ -0,0 +1,39 @@
+use serde::Deserialize;
+
+use crate::broker_statement::partial::PartialBrokerStatement;
+use crate::core::GenericResult;
+use crate::currency::Cash;
+use crate::types::Decimal;
+
+#[derive(Deserialize)]
+pub struct AccountSummary {
+    #[serde(rename = "item")]
+    cash_assets: Vec<CashAssets>,
+}
+
+#[derive(Deserialize)]
+struct CashAssets {
+    #[serde(rename = "currencycode")]
+    currency: String,
+
+    #[serde(rename = "amountin")]
+    start_amount: Decimal,
+
+    #[serde(rename = "amountplaneout")]
+    end_amount: Decimal,
+}
+
+impl AccountSummary {
+    pub fn parse(&self, statement: &mut PartialBrokerStatement) -> GenericResult<bool> {
+        let mut has_starting_assets = false;
+
+        for cash_assets in &self.cash_assets {
+            has_starting_assets |= !cash_assets.start_amount.is_zero();
+
+            let current_amount = Cash::new(&cash_assets.currency, cash_assets.end_amount);
+            statement.assets.cash.as_mut().unwrap().deposit(current_amount);
+        }
+
+        Ok(has_starting_assets)
+    }
+}
--- src/broker_statement/open/spb/mod.rs
+++ src/broker_statement/open/spb/mod.rs
@@ -1,24 +1,36 @@
+mod cash_assets;
+
 use serde::Deserialize;
 
+use crate::broker_statement::open::common::deserialize_date;
 use crate::broker_statement::partial::PartialBrokerStatement;
 use crate::core::GenericResult;
-// use crate::types::Date;
+use crate::types::Date;
 
-// use common::deserialize_date;
+use cash_assets::AccountSummary;
 
 #[derive(Deserialize)]
 pub struct BrokerReport {
-    // #[serde(deserialize_with = "deserialize_date")]
-    // date_from: Date,
+    #[serde(deserialize_with = "deserialize_date")]
+    date_from: Date,
 
-    // #[serde(deserialize_with = "deserialize_date")]
-    // date_to: Date,
+    #[serde(deserialize_with = "deserialize_date")]
+    date_to: Date,
+
+    #[serde(rename = "account_totally_line")]
+    account_summary: AccountSummary,
 }
 
 impl BrokerReport {
     pub fn parse(&self) -> GenericResult<PartialBrokerStatement> {
-        let statement = PartialBrokerStatement::new(true);
-        // statement.period = Some((self.date_from, self.date_to.succ()));
+        let mut statement = PartialBrokerStatement::new(true);
+        statement.period = Some((self.date_from, self.date_to.succ()));
+
+        self.account_summary.parse(&mut statement)?;
+
+        // FIXME(konishchev): Support
+        // statement.set_has_starting_assets(has_starting_assets)
+
         Ok(statement)
     }
 }
