b7997ccbad4f9342cc792af630d6e95f173f82f6
Dmitry Konishchev <konishchev@gmail.com>
2023-03-05 21:41:07 +0300

    Support uppercase extension for BCS statements

--- src/broker_statement/bcs/mod.rs
+++ src/broker_statement/bcs/mod.rs
@@ -34,7 +34,9 @@ impl StatementReader {
 
 impl BrokerStatementReader for StatementReader {
     fn check(&mut self, path: &str) -> GenericResult<bool> {
-        Ok(path.ends_with(".xls"))
+        Ok(path
+            .to_lowercase() // Custom statements have uppercase extension
+            .ends_with(".xls"))
     }
 
     fn read(&mut self, path: &str, _is_last: bool) -> GenericResult<PartialBrokerStatement> {
