40afbfbd4ac0f5c1f15b34484c97ee3081e52d2e
Dmitry Konishchev <konishchev@gmail.com>
2024-06-21 21:51:48 +0300

    Move all tax logic to tax calculator

--- src/taxes/calculator.rs
+++ src/taxes/calculator.rs
@@ -1,7 +1,7 @@
 use crate::core::GenericResult;
 use crate::currency::Cash;
 use crate::localities::Country;
-use crate::taxes::IncomeType;
+use crate::taxes::{self, IncomeType};
 
 pub struct Tax {
     pub expected: Cash,
@@ -43,10 +43,12 @@ impl TaxCalculator {
         }
     }
 
+    // FIXME(konishchev): Won't work for deductible income
+    // FIXME(konishchev): Implement
     pub fn tax_income_dry_run(&self, income_type: IncomeType, year: i32, income: Cash, paid_tax: Option<Cash>) -> Tax {
-        let expected = self.country.tax_to_pay(income_type, year, income, None);
+        let expected = self.tax_to_pay(income_type, year, income, None);
         let paid = paid_tax.unwrap_or_else(|| Cash::zero(self.country.currency));
-        let to_pay = self.country.tax_to_pay(income_type, year, income, paid_tax);
+        let to_pay = self.tax_to_pay(income_type, year, income, paid_tax);
 
         Tax {
             expected: expected,
@@ -56,13 +58,13 @@ impl TaxCalculator {
         }
     }
 
-    pub fn tax_agent_income(&mut self, _income_type: IncomeType, _year: i32, _income: Cash, paid_tax: Cash) -> GenericResult<Tax> {
+    pub fn tax_agent_income(&mut self, _income_type: IncomeType, _year: i32, _income: Cash, mut paid_tax: Cash) -> GenericResult<Tax> {
         if paid_tax.currency != self.country.currency {
             return Err!("Got withheld tax in an unexpected currency: {}", paid_tax.currency)
         }
 
         let provided_paid_tax = paid_tax;
-        let paid_tax = self.country.round_tax(paid_tax);
+        paid_tax.amount = taxes::round_tax(paid_tax.amount, self.country.jurisdiction.traits().tax_precision);
 
         if provided_paid_tax != paid_tax {
             return Err!("Got an unexpected withheld tax: {} vs {}", provided_paid_tax, paid_tax);
@@ -75,5 +77,33 @@ impl TaxCalculator {
             deduction: paid_tax,
             to_pay: Cash::zero(self.country.currency),
         })
+    }
+
+    // FIXME(konishchev): Implement
+    fn tax_to_pay(
+        &self, income_type: IncomeType, year: i32, income: Cash, paid_tax: Option<Cash>,
+    ) -> Cash {
+        assert_eq!(income.currency, self.country.currency);
+
+        let income = income.round();
+        if income.is_negative() || income.is_zero() {
+            return Cash::zero(self.country.currency);
+        }
+
+        let tax_to_pay = self.country.tax_rate(year).tax(income_type, income.amount);
+
+        if let Some(paid_tax) = paid_tax {
+            assert!(!paid_tax.is_negative());
+            assert_eq!(paid_tax.currency, self.country.currency);
+            let tax_deduction = taxes::round_tax(paid_tax.amount, self.country.jurisdiction.traits().tax_precision);
+
+            if tax_deduction < tax_to_pay {
+                self.country.cash(tax_to_pay - tax_deduction)
+            } else {
+                self.country.cash(dec!(0))
+            }
+        } else {
+            self.country.cash(tax_to_pay)
+        }
     }
 }
