7c9b960e3de5ad55ebdf6856c3784766e8d736ae
Dmitry Konishchev <konishchev@gmail.com>
2024-06-21 21:40:58 +0300

    Move round_tax() functoin

--- src/taxes/mod.rs
+++ src/taxes/mod.rs
@@ -10,7 +10,9 @@ use serde::de::{Deserializer, Error};
 
 use crate::brokers::Broker;
 use crate::core::EmptyResult;
+use crate::currency;
 use crate::localities::Jurisdiction;
+use crate::types::Decimal;
 
 pub use self::calculator::{TaxCalculator, Tax};
 pub use self::long_term_ownership::{
@@ -59,4 +61,41 @@ pub fn validate_tax_exemptions(broker: Broker, exemptions: &[TaxExemption]) -> E
     }
 
     Ok(())
+}
+
+// When we work with taxes in Russia, the following rounding rules are applied:
+// 1. Result of all calculations must be with kopecks precision
+// 2. If we have income in foreign currency then:
+//    2.1. Round it to cents
+//    2.2. Convert to rubles using precise currency rate (65.4244 for example)
+//    2.3. Round to kopecks
+// 3. Taxes are calculated with rouble precision. But we should use double rounding here:
+//    calculate them with kopecks precision first and then round to roubles.
+//
+// Декларация program allows to enter income only with kopecks precision - not bigger.
+// It calculates tax for $10.64 income with 65.4244 currency rate as following:
+// 1. income = round(10.64 * 65.4244, 2) = 696.12 (696.115616 without rounding)
+// 2. tax = round(round(696.12 * 0.13, 2), 0) = 91 (90.4956 without rounding)
+pub fn round_tax(tax: Decimal, precision: u32) -> Decimal {
+    currency::round_to(currency::round(tax), precision)
+}
+
+#[cfg(test)]
+mod tests {
+    use rstest::rstest;
+    use super::*;
+
+    #[rstest(tax, expected,
+        case("13",      "13"),
+        case("13.0000", "13"),
+        case("13.1111", "13"),
+        case("13.4949", "13"),
+        case("13.4950", "14"),
+        case("13.9999", "14"),
+    )]
+    fn tax_rounding(tax: &str, expected: &str) {
+        let tax = tax.parse().unwrap();
+        let result = round_tax(tax, Jurisdiction::Russia.traits().tax_precision);
+        assert_eq!(result, expected.parse().unwrap());
+    }
 }
